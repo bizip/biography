9d2b16842395dbcc80110cfa6678272e2cb9ecac
bizip <bizip04@gmail.com>
2022-05-15 17:29:49 +0200

    Fix popup issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,24 +6,25 @@ import Popup from "./Popup";
 
 const NavBar = () => {
     const [click, setclick] = useState(false)
-    const toggleChecked = () => setclick(value => !value);
-    console.log(click)
+    const toggleChecked = () => setclick(prev => !prev);
     return (
-        <nav>
-            { click && <Popup />}
-            <div className="nav__logo">
-                <h1>Pascal</h1>
-            </div>
-            <ul className="nav__links">
-                <li className="desktop"><a href="#Paractice">Practice Areas</a></li>
-                <li className="desktop"><a href="#Paractice">People</a></li>
-                <li className="desktop"><a href="#Paractice">Contact Us</a></li>
-                <li className="desktop"><a href="#Paractice">Careers</a></li>
-                <li className="desktop"><a href="#Paractice">Studio</a></li>
-                <li><a href="#Paractice"><img src={linkedin} className="white" alt="Linkedin" /></a></li>
-                <li><img src={menu} className="white" onClick={toggleChecked} alt="menu" /></li>
-            </ul>
-        </nav>
+        <div>
+            {click ? <Popup /> : <nav>
+                <div className="nav__logo">
+                    <h1>Pascal</h1>
+                </div>
+                <ul className="nav__links">
+                    <li className="desktop"><a href="#Paractice">Practice Areas</a></li>
+                    <li className="desktop"><a href="#Paractice">People</a></li>
+                    <li className="desktop"><a href="#Paractice">Contact Us</a></li>
+                    <li className="desktop"><a href="#Paractice">Careers</a></li>
+                    <li className="desktop"><a href="#Paractice">Studio</a></li>
+                    <li><a href="#Paractice"><img src={linkedin} className="white" alt="Linkedin" /></a></li>
+                    <li><img src={menu} className="white" onClick={toggleChecked} alt="menu" /></li>
+                </ul>
+            </nav>}
+        </div>
+
     )
 }
 export default NavBar;
